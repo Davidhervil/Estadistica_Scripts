45c804eb27c0841d27926a09689cd827dd146668
Elechero <davidhervil@hotmail.com>
2016-03-17 10:32:13 -0430

    Update script.r

--- script.r
+++ script.r
@@ -1,5 +1,5 @@
 #Ejercicio 1
-#a)
+############a)
 prueba_M_varDesc_dist = function(x, m0,alpha) {
   n = length(x)
   media = mean(x)
@@ -15,7 +15,7 @@ OxAz_otono=oxido_azufre[Temp=="otono"]
 
 prueba_M_varDesc_dist(OxAz_otono,2,0.1)
 
-#b)
+###########b)
 prueba_diffM_varDesc_dist = function(x,x2,alpha) {
   n = length(x)
   n2 = length(x2)
@@ -34,4 +34,15 @@ Ne_invierno = Ne[Temp == "invierno"]
 
 prueba_diffM_varDesc_dist(Ne_primavera,Ne_invierno,0.05)
 
-#c)
+###########c)
+prueba_iguVar_mayor = function(x,x2,alpha) {
+  n = length(x)
+  n2 = length(x2)
+  media = mean(x)
+  media2 = mean(x2)
+  Scsq = var(x)
+  Sc2sq = var(x2)
+  z = (media - media2)/sqrt((Scsq/n)+(Sc2sq/n2))
+  RR = qnorm(alpha/2,lower.tail = FALSE)
+  return (c(z,RR))
+}
